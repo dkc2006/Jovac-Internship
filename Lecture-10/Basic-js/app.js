49edb538d0733022d6b9424f8899b92d96612ad3
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-07-01 22:22:08 +0530

    Lecture-10 codes

---
+++ Lecture-10/Basic-js/app.js
@@ -0,0 +1,58 @@
+// console.log("Start with js");
+
+//String
+
+let firstName = "Deepanshu";
+// let lastName = "Khandelwal";
+let age = 21;
+// console.log(firstName);
+// console.log(lastName);
+
+//Concatenate
+
+// console.log(firstName + lastName);
+
+let spac = " ";
+// console.log(firstName + spac + lastName);
+
+// console.log(firstName + " " + lastName);
+
+//backeticks
+
+// console.log(`my firstname is ${firstName} and lastName is ${lastName} and my age is${age}`)
+
+//length
+
+// console.log(firstName.length);
+
+//uppercase()
+//lowercase()
+
+// console.log(firstName.toUpperCase());
+// console.log(firstName.toLowerCase());
+
+let lastName = "       Khandelwal       ";
+let ans = lastName.trim();
+
+console.log(lastName.length);
+console.log(ans);
+console.log(ans.length);
+
+// console.log(first);
+
+//number
+
+let a =10;
+let b = 45.4;
+let c = 45.55654104;
+let d = 2+5;
+let e = 2*10;
+let f = 2%4;
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
