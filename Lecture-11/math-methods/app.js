c816e3f124811a0c10fab029284bd548e9f18073
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-07-02 13:46:58 +0530

    Lecture-11 codes

---
+++ Lecture-11/math-methods/app.js
@@ -0,0 +1,22 @@
+//console.log(Math);
+
+//-------------
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// function counter(arr){
+//     for(let item =0;item<10;item++){
+//         console.log(arr[item])
+//     }
+// }
+
+//counter(arr)
+
+//for-of  => array
+//for-in => objects
+
+let arr = [1,2,3,4,5,6,7,8,9,10]
+function counter(arr){
+    for(let item =0;item<10;item++){
+        console.log(arr[item])
+    }
+}
