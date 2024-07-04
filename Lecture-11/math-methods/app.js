501e664671598af82eeeed007c94aa82438a0974
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-07-05 00:41:09 +0530

    Lecture-11 codes

--- Lecture-11/math-methods/app.js
+++ Lecture-11/math-methods/app.js
@@ -1,22 +1,128 @@
-//console.log(Math);
+
+// console.log(Math);
+
+// if(true){
+//     console.log("true");
+// }else{
+//     console.log("false");
+    
+// }
+
+//--------------------------
+
+// if(true){
+//     console.log(true);
+// }else{
+//     console.log(false);
+    
+// }
 
 //-------------
 
-// let arr = [1,2,3,4,5,6,7,8,9,10]
-// function counter(arr){
-//     for(let item =0;item<10;item++){
-//         console.log(arr[item])
-//     }
+// let score = 34;
+// let final = score == 100;
+// console.log(final)
+
+// == -> value check karo
+
+// === -> value check karta hai + datatype(primitive)
+
+
+// let a = 1;
+
+// let b = '1';
+// console.log(a==b); //General Checking/Equality
+// console.log(a===b); // Strict Checking/Equality
+
+// let num = 10;
+// let final = num === '10';
+// if(final){
+//     console.log("i am Deepu");
+// }else{
+//     console.log("I am Kanu");
 // }
 
-//counter(arr)
+//not -> !
 
-//for-of  => array
+// let num = 10;
+// let final = num != '10';
+// if(final){
+//     console.log("i am Deepu");
+// }else{
+//     console.log("I am Kanu");
+// }
+// console.log(100>=99)
+// console.log(100>=100)
+// console.log(100==100)
+// console.log(100<=100)
+// console.log(100<=101)
+// console.log(100<101)
+// console.log(102>101)
+
+// --------------------
+
+
+// function deepanshu(){
+//     console.log("hello sam")
+// }
+
+// deepanshu()
+// deepanshu()
+// deepanshu()
+
+// ------
+
+// function samy(a,b){
+//     console.log(a)
+//     console.log(b)
+//     console.log(a+b)
+// }
+// samy(10,20)
+// samy(30,50)
+
+
+// -------------------------
+// let person = {
+//     naam:"swaggy boi",
+//     umar: 24,
+//     lang : "punjabi",
+//     favDialogue : function(){}
+// }
+
+// console.log(Math)
+
+// -------
+
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+
+// function counter(arr){
+//     for(let item=0;item<10;item++){
+//        console.log(arr[item])
+//     }
+// }
+// counter(arr)
+
+//for-of => array
 //for-in => objects
 
-let arr = [1,2,3,4,5,6,7,8,9,10]
-function counter(arr){
-    for(let item =0;item<10;item++){
-        console.log(arr[item])
-    }
-}
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+
+// function counter(arr){
+//    for(let item of arr){
+//     console.log(item)
+//    }
+// }
+// counter(arr)
+// -------
+
+// let obj = {
+//     a:10,
+//     b:20,
+//     c:30,
+//     d:40
+// }
+
+// for(let item in obj){
+//     console.log(item)
+//     console.log(obj[item])
+/
