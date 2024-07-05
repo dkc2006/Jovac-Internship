6f6d406e1ac76f27c1ed8322325aca10770cffc5
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-07-05 14:11:03 +0530

    lecture-12 codes

---
+++ Lecture-12/functions/app.js
@@ -0,0 +1,60 @@
+// syntax
+
+// function deep(){
+
+// }
+
+//deep()
+
+// -------------------
+
+// function deep(){
+//     return 10;
+// }
+
+//camel casing -> let deepanshuKhandelwalISACoder
+
+// let returnedValue = deep();
+// console.log(returnedValue);
+
+// -------------------------------
+
+// parameterised function
+
+// function deep(a,b){
+//     let c = a+b;
+//     return c;
+// }
+
+// camel casing -> let deepanshuKhandelwalISACoder
+
+// let returnedValue = deep(10,30);//10
+// console.log(returnedValue);
+
+// ----------------------------------
+
+// function output(c,d){
+//     let a = 10;
+//     let b;
+    // when nothing is returned from a function then by default ``undefined`` is returned
+// }
+// console.log(output(11,21))
+
+// ----------------------------
+
+// function output(c,d){
+//     let a = 10;
+//     let b = NaN;
+    // let e = undefined;
+//     return a+b+c+d;
+// }
+// console.log(output(11,21));
+
+
+// console.log(typeof(10));//number
+// console.log(typeof(true));//Boolean
+// console.log(typeof("10"));//string
+
+// console.log(typeof(undefined));//undefined
+// console.log(typeof(null));//Obj
+// console.log(typeof(NaN));//num
