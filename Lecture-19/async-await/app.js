496de8859532df829eff1b7c725c8f606246e9b4
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-07-22 21:47:20 +0530

    Lecture-19 codes

--- Lecture-19/async-await/app.js
+++ Lecture-19/async-await/app.js
@@ -1,24 +1,74 @@
+//when nothing is returned from a fn then by default undefined is returned
+// function sam(){
+
+// }
+
+// let output = sam()
+
+// console.log(output) //undefined
+
+// ----------------------
+
+// function sam(){
+
+// }
+
+// let output = new sam() //constructor -> object
+
+// console.log(output) //empty object
 
 
-function deep(){
+// ----------------------
 
-}
+// async function sam(){
 
-let output = deep();
+//     return 10
+// }
 
-console.log(output); //undefined
+// let output = sam() 
+
+// console.log(output) // promise always
+
+// ----------------------
 
 
-// ----------------------------------------------------------------
+// async function sam(){
+//         console.log("1");
+//         setTimeout(()=>{
+//             console.log("settimeout-1")
+//         } , 2000)
+//         console.log("2")
+// }
+
+// async function sam2(){
+//     console.log("1");
+//     console.log("2")
+// }
+
+// sam()
+// sam2()
+
+// -----------------
+
+// async function sam(){
+//     console.log("1");
+//     let data = await fetch('https://api.tvmaze.com/search/shows?q=girls')
+//     console.log(data);
+//     console.log("2")
+// }
+
+// async function sam2(){
+//     console.log("1");
+//     console.log("2")
+// }
+
+// sam()
+// sam2()
+
+// ----------------
+
+// api calling
+// fetch and axios
 
 
-async function deep(){
-    console.log("1");
-    setTimeout(()=>{
-        console.log("setTimeout-1")
-    },2000)
-    console.log("2");
-}
-
-console.log("2");
 
