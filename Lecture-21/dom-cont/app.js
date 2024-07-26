5b3c15eb1bcae011555b08d68a9aa4f17adf304f
Deepanshu Khandelwal <khandelwaldeepanshu6@gmail.com>
2024-07-26 23:00:29 +0530

    Lecture-21 codes

--- Lecture-21/dom-cont/app.js
+++ Lecture-21/dom-cont/app.js
@@ -1,7 +1,48 @@
+// let h1 = document.getElementsByTagName('h1');
+// let classH1 = document.getElementsByClassName('deep');
+// let idH1 = document.getElementById('deepu');
 
+// let q1 = document.querySelector('h1')
+// let q2 = document.querySelector('.deep')
+// let q3 = document.querySelector('#deepu')
+
+
+
+// q1.style.color = 'blue'
+// q1.style.color = 'red'
+// q2.style.backgroundColor = "orange"
+
+// -----------------------
+
+// h1.style.color = "red"
+
+//wet (write everything twice)
+// for(let heading of h1){
+//     heading.style.color = 'red';
+//     heading.style.backgroundColor = 'green';
+//     heading.style.fontSize = '24px';
+// }
+
+// dry (donot repeat yourself)
+// for(let heading of h1){
+//     heading.style.cssText = `
+//     font-size:50px;
+//     color:pink;
+//     background-color:blue
+//     `
+// }
+
+
+// ---------------
+
+// get an attribute
+// let a = document.querySelector('a')
+let inp = document.querySelector('input')
+// console.log(a.getAttribute('href'))s
+console.log(inp.getAttribute('type'))
 
 
 //set an attribute
-
 let a = document.querySelector('a')
-
+a.setAttribute('href' , 'http://youtubee.com')
+console.log(a.getAttribute('href'))
